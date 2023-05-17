947f002077536926beac404bdcacaf9c04af3f7b
PangLAN <i@yinn.party>
2023-05-17 22:48:50 +0800

    Update

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
 ;; (require 'init-benchmarking) ;; Measure startup time
 
 ;; Adjust garbage collection thresholds during startup, and thereafter
-
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
