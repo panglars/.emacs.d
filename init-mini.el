e26231f5fe7f14ec848af23001a04fb122d50f7f
PangLAN <i@yinn.party>
2023-09-14 10:43:12 +0800

    Add minimal config

---
+++ init-mini.el
@@ -0,0 +1,40 @@
+;;; init-mini.el --- Emacs minimal configurations.	-*- lexical-binding: t no-byte-compile: t -*-
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(setq straight--process-log nil
+      straight-cache-autoloads t
+      straight-repository-branch "develop"
+      straight-enable-package-integration nil
+      straight-vc-git-default-clone-depth 1
+      straight-use-package-by-default t
+      use-package-always-ensure nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package
+(setq use-package-enable-imenu-support t
+      use-package-verbose (not (bound-and-true-p byte-compile-current-file))
+      use-package-expand-minimally t
+      use-package-compute-statistics nil)
+(setq byte-compile-warnings '(cl-functions))
+(straight-use-package 'use-package)
+
+;; Load core config
+(require 'init-const)
+(require 'init-base)
+(require 'init-core)
+
+(require 'init-meow)
+(require 'init-ui)
