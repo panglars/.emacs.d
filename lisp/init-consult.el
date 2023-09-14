dac72a851e4a02bf658be9a749ba1dcb37e8fc8b
PangLAN <i@yinn.party>
2023-09-14 11:39:36 +0800

    Add more keybings in consult-mode

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -1,6 +1,20 @@
 (use-package consult
-  :bind
-  ("C-x C-r" . consult-recent-file))
+  :bind(;; C-c bindings in `mode-specific-map'
+        ("C-c M-x" . consult-mode-command)
+        ("C-c b"   . consult-buffer)
+        ("C-c r" . consult-ripgrep)
+        ("C-c R" . consult-bookmark)            ;; orig. bookmark-jump
+        ("C-x b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+        ("C-x C-r" . consult-recent-file)
+        ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+        ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+        ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+        ("M-#"   . consult-register-load)
+        ("M-'"   . consult-register-store)        ;; orig. abbrev-prefix-mark (unrelated)
+        ("C-M-#" . consult-register)
+        ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+        )
+  )
 
 
 (provide 'init-consult)
