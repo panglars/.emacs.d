f93d98d4ec71682b532b2e2ff2b9e62d9cefefab
PangLAN <i@yinn.party>
2023-09-25 22:07:14 +0800

    remove scss-mode,php-mode

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -2,15 +2,6 @@
 (use-package css-mode
   :init (setq css-indent-offset 2))
 
-;; SCSS
-(use-package scss-mode
-  :init
-  (setq scss-compile-at-save nil))
-
-;; LESS
-(unless (fboundp 'less-css-mode)
-  (use-package less-css-mode))
-
 ;; Major mode for editing web templates
 (use-package web-mode
   :mode "\\.\\(phtml\\|php\\|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
@@ -22,9 +13,6 @@
 ;; Adds node_modules/.bin directory to `exec_path'
 (use-package add-node-modules-path
   :hook ((web-mode js-mode js2-mode) . add-node-modules-path))
-
-(use-package haml-mode)
-(use-package php-mode)
 
 (use-package restclient
   :mode ("\\.http\\'" . restclient-mode)
