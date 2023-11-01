b27763eca6da4ec665caf4acb90402539a76bef0
PangLAN <i@yinn.party>
2023-11-01 22:01:41 +0800

    Update prog

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,26 +1,8 @@
-(use-package nxml-mode
-  :straight (:type built-in)
-  :mode (("\\.xml\\'" . nxml-mode)
-         ("\\.rss\\'" . nxml-mode))
-  :custom
-  (nxml-slash-auto-complete-flag t)
-  (nxml-auto-insert-xml-declaration-flag t))
-
-(use-package toml-mode
-  :mode (("\\.toml$" . toml-mode)))
-
-(use-package yaml-mode
-  :mode "\\.yml\\'"
-  :init
-  (setq yaml-indent-offset 2))
-
-(use-package dockerfile-mode
-  :mode "\\Dockerfile\\'")
-
-(use-package lua-mode
-  :mode "\\.lua$"
-  :init
-  (setq lua-indent-level 2))
+(setq major-mode-remap-alist
+      '((yaml-mode . yaml-ts-mode)
+        (bash-mode . bash-ts-mode)
+        (json-mode . json-ts-mode)
+        (css-mode . css-ts-mode)))
 
 (use-package quickrun
   :custom
@@ -29,3 +11,4 @@
   (("<f5>" . quickrun)
    ("M-<f5>" . quickrun-shell)))
 (provide 'init-prog)
+
