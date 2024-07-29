aa81487fd31864c6e43a7cf4d1ed10b752ac4a11
PangLAN <i@pll.moe>
2024-07-29 16:30:28 +0800

    update ibuffer

--- lisp/init-buffer.el
+++ lisp/init-buffer.el
@@ -1,8 +1,10 @@
 (use-package ibuffer
-  :disabled
-  :init
+  :straight (:type built-in)
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
+  :config
   (use-package nerd-icons-ibuffer)
   (use-package ibuffer-vc
+    :disabled
     :commands (ibuffer-vc-set-filter-groups-by-vc-root)
     :custom
     (ibuffer-vc-skip-if-remote 'nil))
@@ -10,60 +12,6 @@
   :custom
   (ibuffer-expert t)
   (ibuffer-movement-cycle nil)
-  (ibuffer-show-empty-filter-groups nil)
-  ;; / R:switch to saved filter groups
-  (ibuffer-saved-filter-groups
-   '(("Default"
-      ("Emacs" (or (name . "\\*scratch\\*")
-                   (name . "\\*dashboard\\*")
-                   (name . "\\*compilation\\*")
-                   (name . "\\*Backtrace\\*")
-                   (name . "\\*Packages\\*")
-                   (name . "\\*Messages\\*")
-                   (name . "\\*Customize\\*")))
-      ("Browser" (or (mode . eww-mode)
-                     (mode . xwidget-webkit-mode)))
-      ("Help" (or (name . "\\*Help\\*")
-                  (name . "\\*Apropos\\*")
-                  (name . "\\*info\\*")
-                  (mode . Man-mode)
-                  (mode . woman-mode)))
-      ("Repl" (or (mode . gnuplot-comint-mode)
-                  (mode . inferior-emacs-lisp-mode)
-                  (mode . inferior-python-mode)))
-      ("Term" (or (mode . term-mode)
-                  (mode . shell-mode)
-                  (mode . eshell-mode)))
-      ("Mail" (or (mode . mail-mode)
-                  (mode . message-mode)
-                  (derived-mode . gnus-mode)))
-      ("Conf" (or (mode . yaml-mode)
-                  (mode . conf-mode)))
-      ("Dict" (or (mode . fanyi-mode)
-                  (mode . dictionary-mode)))
-      ("Text" (and (derived-mode . text-mode)
-                   (not (starred-name))))
-      ("Magit" (or (mode . magit-repolist-mode)
-                   (mode . magit-submodule-list-mode)
-                   (mode . git-rebase-mode)
-                   (derived-mode . magit-section-mode)
-                   (mode . vc-annotate-mode)))
-      ("VC" (or (mode . diff-mode)
-                (derived-mode . log-view-mode)))
-      ("Prog" (and (derived-mode . prog-mode)
-                   (not (starred-name))))
-      ("IRC" (or (mode . rcirc-mode)
-                 (mode . erc-mode))))))
-  (ibuffer-formats
-   '((mark modified read-only locked " "
-           (name 35 35 :left :elide)
-           " "
-           (size 9 -1 :right)
-           " "
-           (mode 16 16 :left :elide)
-           " " filename-and-process)
-     (mark " "
-           (name 16 -1)
-           " " filename))))
+  (ibuffer-show-empty-filter-groups nil))
 
 (provide 'init-buffer)
