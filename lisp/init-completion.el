0c6041b262aec94d2bd46f2a9c27eeda2a10d493
PangLAN <i@pll.moe>
2025-04-13 19:56:58 +0800

    use lsp-mode

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -4,7 +4,6 @@
 (setq text-mode-ispell-word-completion nil)
 
 (use-package corfu
-  :disabled
   :straight (corfu :includes (corfu-indexed corfu-quick) :files (:defaults "extensions/corfu-*.el"))
   :custom
   (corfu-auto t)
