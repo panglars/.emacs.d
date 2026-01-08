e052f015ab83504c5deaecc4374745cc5484c265
PangLARS <i@panglars.cc>
2026-01-09 02:48:15 +0800

    add agent-shell and vterm

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -1,4 +1,10 @@
 ;;  -*- lexical-binding: t; -*-
 
+(use-package agent-shell
+  :config
+  (setq agent-shell-google-authentication
+        (agent-shell-google-make-authentication :login t))
+  )
+
 
 (provide 'init-ai)
